be45fdf3b64a31fb3683e6f8ffeedff85332e4be
ysjeung <yunsukjeung@snu.ac.kr>
2025-02-11 01:07:05 +0900

    ltry¯

--- boards/shields/nice_view_gem/assets/crystal.c
+++ boards/shields/nice_view_gem/assets/crystal.c
@@ -88,7 +88,7 @@ const LV_ATTRIBUTE_MEM_ALIGN LV_ATTRIBUTE_LARGE_CONST LV_ATTRIBUTE_IMG_CRYSTAL_0
 };
 
 const lv_img_dsc_t crystal_00 = {
-  .header.cf = LV_IMG_CF_ALPHA_1BIT,
+  .header.cf= LV_IMG_CF_INDEXED_1BIT,
   .header.always_zero = 0,
   .header.reserved = 0,
   .header.w = 126,

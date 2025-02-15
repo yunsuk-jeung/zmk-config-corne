341b3af219859f70bb18c52451be007e16eb21c9
ysjeung <yunsukjeung@snu.ac.kr>
2025-02-15 15:31:16 +0900

    back to before change image

--- boards/shields/nice_view_gem/widgets/animation.c
+++ boards/shields/nice_view_gem/widgets/animation.c
@@ -31,15 +31,15 @@ const lv_img_dsc_t *anim_imgs[] = {
 };
 
 void draw_animation(lv_obj_t *canvas) {
-// #if IS_ENABLED(CONFIG_NICE_VIEW_GEM_ANIMATION)
-//     lv_obj_t *art = lv_animimg_create(canvas);
-//     lv_obj_center(art);
+#if IS_ENABLED(CONFIG_NICE_VIEW_GEM_ANIMATION)
+    lv_obj_t *art = lv_animimg_create(canvas);
+    lv_obj_center(art);
 
-//     lv_animimg_set_src(art, (const void **)anim_imgs, 2);
-//     lv_animimg_set_duration(art, CONFIG_NICE_VIEW_GEM_ANIMATION_MS);
-//     lv_animimg_set_repeat_count(art, LV_ANIM_REPEAT_INFINITE);
-//     lv_animimg_start(art);
-// #else
+    lv_animimg_set_src(art, (const void **)anim_imgs, 2);
+    lv_animimg_set_duration(art, CONFIG_NICE_VIEW_GEM_ANIMATION_MS);
+    lv_animimg_set_repeat_count(art, LV_ANIM_REPEAT_INFINITE);
+    lv_animimg_start(art);
+#else
     lv_obj_t *art = lv_img_create(canvas);
 
     int length = sizeof(anim_imgs) / sizeof(anim_imgs[0]);
@@ -47,8 +47,8 @@ void draw_animation(lv_obj_t *canvas) {
     int random_index = rand() % length;
 
     lv_img_set_src(art, anim_imgs[random_index]);
-// #endif
+#endif
 
-    // lv_obj_align(art, LV_ALIGN_TOP_LEFT, -14, 0);
-    lv_obj_align(art, LV_ALIGN_TOP_LEFT, 0, 0);
+    lv_obj_align(art, LV_ALIGN_TOP_LEFT, -14, 0);
+
 }
